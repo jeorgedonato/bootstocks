92bf75e782358f116141fca1421e73815794e37d
jeorgedonato <jeorgekhenr@gmail.com>
2022-02-12 21:35:09 -0800

    Still working on seeds and cors

---
+++ migrations/20220213044015-create-user.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     await queryInterface.createTable('user', { 
+       id: Sequelize.INTEGER,
+       username: Sequelize.STRING,
+       password: Sequelize.STRING,
+       firtname: Sequelize.STRING,
+       lastname: Sequelize.STRING,
+       profile_pic: Sequelize.STRING,
+       user_money: Sequelize.FLOAT,
+      });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('user')
+  }
+};
